a838a3ae9eccfa1fc03553fb631a8e4d6f91a26c
Jang Hong Jun <37576725+ajang369@users.noreply.github.com>
2023-11-25 02:24:03 +0900

    [Bronze II] Title: 문자열 분석, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/10820. 문자열 분석/문자열 분석.py
@@ -0,0 +1,20 @@
+import sys
+input = sys.stdin.readline
+while 1:
+    line = input().strip('\n')
+
+    if not line:
+        break
+
+    l, u, d, s = 0, 0, 0, 0
+    for each in line:
+        if each.islower():
+            l += 1
+        elif each.isupper():
+            u += 1
+        elif each.isdigit():
+            d += 1
+        elif each.isspace():
+            s += 1
+
+    print(l, u, d, s)
