4b68280e0d8728213e2763e75a1f137fb142842f
Jang Hong Jun <37576725+ajang369@users.noreply.github.com>
2024-08-08 09:38:02 +0900

    [D4] Title: [S/W 문제해결 기본] 4일차 - 괄호 짝짓기, Time: 118 ms, Memory: 18,368 KB -BaekjoonHub

---
+++ SWEA/D4/1218. ［S／W 문제해결 기본］ 4일차 － 괄호 짝짓기/［S／W 문제해결 기본］ 4일차 － 괄호 짝짓기.java
@@ -0,0 +1,59 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Solution {
+	public static boolean isValid(String s) {
+		Stack<Character> stack = new Stack<>();
+		for (char c : s.toCharArray()) {
+			if (c=='(' || c=='[' || c=='{' || c=='<') {
+				stack.push(c);
+			}
+			else if (c==')' || c==']' || c=='}' || c=='>') {
+				if (stack.isEmpty())
+					return false;
+				char open = stack.pop();
+				
+				boolean isMatch = isMatchCheck(open, c);
+				if (isMatch == false)
+					return false;
+			}
+		}
+
+		return stack.isEmpty();
+	}
+	
+	private static boolean isMatchCheck(char open, char close) {
+		return (open == '(' && close == ')' ||
+				open == '[' && close == ']' ||
+				open == '{' && close == '}' ||
+				open == '<' && close == '>');
+	}
+
+
+	public static void main(String[] args) throws Exception {
+		StringBuilder sb = new StringBuilder();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		//System.setIn(new FileInputStream("1218_input.txt"));
+		
+		int T = 10;
+		for(int test_case = 1; test_case <= T; test_case++)
+		{
+			sb.append("#" + test_case + " ");
+			int n = Integer.parseInt(br.readLine());
+			
+			String s = br.readLine();
+			
+			boolean result = isValid(s);
+			if (result) {
+				sb.append("1" + "\n");
+			} else {
+				sb.append("0" + "\n");
+			}
+		}
+		System.out.println(sb);
+		
+	}
+}
