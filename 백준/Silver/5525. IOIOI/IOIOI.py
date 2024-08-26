eb8b46270291a67a724262ef43bfb891f32f9088
Jang Hong Jun <37576725+ajang369@users.noreply.github.com>
2024-08-26 19:50:03 +0900

    [Silver I] Title: IOIOI, Time: 312 ms, Memory: 33076 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.py
+++ 백준/Silver/5525. IOIOI/IOIOI.py
@@ -2,19 +2,19 @@ n = int(input())
 m = int(input())
 S = input()
 
-pn_len = n*2 + 1
+idx, count = 0, 0
+result = 0
+while idx < (m-1):
+    # idx부터 길이가 3인곳 까지 IOI인지 확인
+    if S[idx:idx+3] == 'IOI':
+        count += 1
+        idx += 2    # 다음으로 이동 -> +2
 
-tmp = ''
-for i in range(pn_len):
-    if i%2 == 0:
-        tmp += 'I'
+
+        if count == n:
+            result += 1
+            count -= 1
     else:
-        tmp += 'O'
-
-cnt = 0
-for i in range(m):
-    if S[i] == 'I':
-        res = S[i:i+pn_len]
-        if res == tmp:
-            cnt += 1
-print(cnt)
+        idx += 1
+        count = 0
+print(result)
