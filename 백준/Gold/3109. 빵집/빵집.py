6343e9e6029a052b52d571b7022f3f1a018d885f
Jang Hong Jun <37576725+ajang369@users.noreply.github.com>
2024-09-04 22:28:36 +0900

    [Gold II] Title: 빵집, Time: 2664 ms, Memory: 69936 KB -BaekjoonHub

--- 백준/Gold/3109. 빵집/빵집.py
+++ 백준/Gold/3109. 빵집/빵집.py
@@ -6,6 +6,7 @@ for _ in range(r):
     pipe.append(tmp)
 
 def dfs(x, y):
+    # 끝에 도달하면 dfs종료
     if y == c-1:
         return True
 
@@ -14,8 +15,10 @@ def dfs(x, y):
         nx = x+dx[i]
         if 0 <= ny < c and 0 <= nx < r and pipe[nx][ny] != 'x':
             pipe[nx][ny] = 'x'
+            # 열 끝까지 도달했으면 리턴하고 종료
             if dfs(nx, ny):
                 return True
+    # 끝까지 못갔으니까 false
     return False
 
 result = 0
