e7d45e41a962d63ce0e146968e5a1584ad98caf1
Jang Hong Jun <37576725+ajang369@users.noreply.github.com>
2024-10-03 16:51:02 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 10.22 ms, Memory: 80.1 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.java
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.java
@@ -1,11 +1,14 @@
 public class Solution {
     public int[] solution(String s) {
-        int cnt = 0;
-        int zeroCnt = 0;
+        int cnt = 0;        // 변환 횟수
+        int zeroCnt = 0;    // 0의 개수
 
-        while (!s.equals("1")) {
-            zeroCnt += s.length() - s.replace("0", "").length();
+        while (!s.equals("1")) { // s가 1이 될 때까지 반복
+            int initLen = s.length();
             s = s.replace("0", "");
+                
+            zeroCnt += initLen - s.length();
+
             s = Integer.toBinaryString(s.length());
             cnt++;
         }
